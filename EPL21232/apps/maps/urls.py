5e3e23849c45fe60865866999d725109507af0e2
nverbois <38432529+nverbois@users.noreply.github.com>
2021-02-28 17:33:17 +0100

    Premier marqueur sur la carte + lien vers la page de données

--- EPL21232/apps/maps/urls.py
+++ EPL21232/apps/maps/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url                                                                                                                              
+from django.conf.urls import url                                                                                                                           
 from . import views
 
 app_name="map"
 urlpatterns = [ 
-    url(r'', views.default_map, name="map"),
+    url("", views.default_map, name="map"),
 ]
