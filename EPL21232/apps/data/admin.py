c7e459cb291836629699a0328aed54bfff1adfd5
nverbois <38432529+nverbois@users.noreply.github.com>
2021-03-10 12:52:32 +0100

    small fix

--- EPL21232/apps/data/admin.py
+++ EPL21232/apps/data/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Station, Data
+from .models import Station, Data, Mean, Intensity
 from import_export.admin import ImportExportModelAdmin
 
 class StationAdmin(admin.ModelAdmin):
@@ -13,4 +13,6 @@ class DataAdmin(ImportExportModelAdmin):
     list_display = ("tilting_date", "tilting_time", "tilting_number", "tilting_mm")
 admin.site.register(Data, DataAdmin)
 
+admin.site.register(Mean)
+admin.site.register(Intensity)
 
