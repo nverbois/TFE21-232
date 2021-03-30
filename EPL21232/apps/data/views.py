e9e0e2de6985ad1d319623ab025ed44762864133
nverbois <38432529+nverbois@users.noreply.github.com>
2021-03-30 14:58:14 +0200

    Data Tables update

--- EPL21232/apps/data/views.py
+++ EPL21232/apps/data/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-from .models import Station,Data,MeanDay,Intensity
+from .models import Station,Data,MeanDay,Intensity, MeanWeek, MeanYear
 # Create your views here.
 
     
@@ -10,13 +10,19 @@ def dynamic_lookup_view(request: HttpRequest, my_id) -> HttpResponse:
     # 10 latests data for the station
     data = Data.objects.order_by('-tilting_date').filter(station=station)[:100][::-1]
     # 10 latests means for the station (counting per day !!!)
-    meandaytable = MeanDay.objects.filter(station=station).order_by('-mean_day')[:10][::-1]
+    meandaytable = MeanDay.objects.filter(station=station).order_by('-mean_day')[::-1]
+    meanweektable = MeanWeek.objects.filter(station=station).order_by('-mean_week')[::-1]
+    meanyeartable = MeanYear.objects.filter(station=station).order_by('-mean_year')[::-1]
+    intensitytable = Intensity.objects.filter(station=station).order_by('-intensity_day')[:10][::-1]
     
     
     context = {
         'id': my_id,
         "data": data,
         "meandaytable": meandaytable,
+        "meanweektable": meanweektable,
+        "meanyeartable": meanyeartable,
+        "intensitytable": intensitytable,
         "station": station
     }
     return render(request, "data-old.html", context)
