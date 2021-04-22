82eb7e1b521ace4354783f4d7c86d9661a379d4d
nverbois <38432529+nverbois@users.noreply.github.com>
2021-04-22 17:16:07 +0200

    Small frontend fixes

--- EPL21232/apps/contact/forms.py
+++ EPL21232/apps/contact/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name = forms.CharField(required=True, widget=forms.TextInput(attrs={'class' : 'form-control', 'placehodler' : "Name"}))
-    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class' : 'form-control', 'placehodler' : "Email"}))
+    nom = forms.CharField(required=True, widget=forms.TextInput(attrs={'class' : 'form-control', 'placehodler' : "Nom complet"}))
+    email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class' : 'form-control', 'placehodler' : "Adresse email"}))
     message = forms.CharField(required=True, widget=forms.Textarea(attrs={'class' : 'form-control', 'placehodler' : "Message", 'rows' : 5}))
