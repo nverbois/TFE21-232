ca5af5b0605b04e49d3e494e0a29dc51f7d66010
duprezf <Florian.duprez@student.uclouvain.be>
2021-05-17 22:59:25 +0200

    test

--- EPL21232/apps/data/tests.py
+++ EPL21232/apps/data/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Station
+from django.utils import timezone
+from django.urls import reverse
+from .forms import CustomImportForm, CustomConfirmImportForm
 
-# Create your tests here.
+class StationTest(TestCase):
+
+    def create_station(self):
+        return Station.objects.create(name="name_test", normalized_name="normalized_name_test", description = "descrpiton_test", longitude = 0.0, latitude = 0.0 )
+
+
+    def test_station_creation(self):
+        station_test = self.create_station()
+        self.assertTrue(isinstance(station_test, Station))
+        self.assertEqual(station_test.__str__(), station_test.name)
